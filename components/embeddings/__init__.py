ddf3a95fdebd7fd88dee76787cc923a8f97db60e
Paul Parkinson <paul.parkinson@oracle.com>
2025-09-01 14:29:44 -0400

    add __init__.py files though they may not be necessary or useful

---
+++ components/embeddings/__init__.py
@@ -0,0 +1,11 @@
+from .similarity import EmbeddingSimilarityComponent
+from .text_embedder import TextEmbedderComponent
+from .local_sentencetransformer import LocalSentenceTransformerComponent
+
+__all__ = [
+    "CloudflareWorkersAIEmbeddingsComponent",
+    "EmbeddingSimilarityComponent",
+    "LocalSentenceTransformerComponent",
+    "MistralAIEmbeddingsComponent",
+    "TextEmbedderComponent",
+]
